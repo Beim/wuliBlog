dafe5b3a31aba7f0837b1a69419958bb75db2ef8
Beim <805908499@qq.com>
2016-05-14 10:50:57 +0800

    add blog(front)

--- public/js/blog.js
+++ public/js/blog.js
@@ -7,7 +7,92 @@ React.initializeTouchEvents(true)
 var total = React.createClass({display: 'total',
 	render : function(){
 		return(
-			rce('div', null, 'blog')
+			rce('div', {'className': 'blog'},
+				rce('div', {'className': 'site-header'},
+					rce('a', {'className': 'site-title', 'href': 'https://github.com/Beim'}, myName),
+					rce('nav', {'className': 'site-nav'}, 
+						rce('a',{'className': 'site-link', 'href': '../index.html'}, 'HOME'),
+						rce('a',{'className': 'site-link', 'href': '../about.html'}, 'ABOUT'),
+						rce('a',{'className': 'site-link', 'href': '../blog.html'}, 'BLOG'),
+						rce('a',{'className': 'site-link', 'href': '#'}, 'TieBa'),
+						rce('a',{'className': 'site-link', 'href': '#'}, 'CharRoom'),
+						rce('a',{'className': 'site-link', 'href': '#'}, 'MORE')
+					)
+				),
+				rce('div', {'className': 'fullstrip'},
+					rce('div', {'className': 'fullstrip-container'},
+						rce('div', {'className': 'fullstrip-title'}, 'Blog')
+					)
+				),
+				rce('div', {'className': 'mainContainer'},
+					rce('div', {'className': 'mainContainer-content'},
+						// rce('div', {'className': '2'}, 'main')
+						rce('div', {'className': 'post-wrap'},
+							rce('h1', {'className': 'post-name'},
+								rce('a', {'href': '#'}, 'Anouncing Event Calendar App')
+							),
+							rce('div', {'className': 'post-date'}, '#2016 05 12'),
+							rce('div', {'className': 'post-excerpt'},
+								rce('p', null, 'I’ve been building side projects since the day that I began programming. However, only more recently have I taken my side projects seriously and tried to build something useful, something that people will actually use. Horu You may or may not know that just under 6 months ago I released...')
+							),
+							rce('div', {'className': 'post-tags'},
+								rce('span', {'className': 'post-tag'}, 'tags:'),
+								rce('span', {'className': 'post-tag'},
+									rce('a', {'href': '#'}, 'html')
+								),
+								rce('span', {'className': 'post-tag'},
+									rce('a', {'href': '#'}, 'html')
+								),
+								rce('span', {'className': 'post-tag'},
+									rce('a', {'href': '#'}, 'html')
+								)
+							)
+						),
+						rce('div', {'className': 'post-wrap'},
+							rce('h1', {'className': 'post-name'},
+								rce('a', {'href': '#'}, 'Anouncing Event Calendar App')
+							),
+							rce('div', {'className': 'post-date'}, '#2016 05 12'),
+							rce('div', {'className': 'post-excerpt'},
+								rce('p', null, 'I’ve been building side projects since the day that I began programming. However, only more recently have I taken my side projects seriously and tried to build something useful, something that people will actually use. Horu You may or may not know that just under 6 months ago I released...')
+							),
+							rce('div', {'className': 'post-tags'},
+								rce('span', {'className': 'post-tag'}, 'tags:'),
+								rce('span', {'className': 'post-tag'},
+									rce('a', {'href': '#'}, 'html')
+								),
+								rce('span', {'className': 'post-tag'},
+									rce('a', {'href': '#'}, 'html')
+								),
+								rce('span', {'className': 'post-tag'},
+									rce('a', {'href': '#'}, 'html')
+								)
+							)
+						),
+						rce('div', {'className': 'post-wrap'},
+							rce('h1', {'className': 'post-name'},
+								rce('a', {'href': '#'}, 'Anouncing Event Calendar App')
+							),
+							rce('div', {'className': 'post-date'}, '#2016 05 12'),
+							rce('div', {'className': 'post-excerpt'},
+								rce('p', null, 'I’ve been building side projects since the day that I began programming. However, only more recently have I taken my side projects seriously and tried to build something useful, something that people will actually use. Horu You may or may not know that just under 6 months ago I released...')
+							),
+							rce('div', {'className': 'post-tags'},
+								rce('span', {'className': 'post-tag'}, 'tags:'),
+								rce('span', {'className': 'post-tag'},
+									rce('a', {'href': '#'}, 'html')
+								),
+								rce('span', {'className': 'post-tag'},
+									rce('a', {'href': '#'}, 'html')
+								),
+								rce('span', {'className': 'post-tag'},
+									rce('a', {'href': '#'}, 'html')
+								)
+							)
+						)
+					)
+				)
+			)
 		)
 	}
 })
